050caa8e67bed800edcad99b2ab519444ecfc5b0
Michel C <michelc@localdomain>
2020-09-27 10:51:42 +0200

    daily check-in v0.1.5.6 ns: (2020-09-27) top: /home/michelc/repo/gitea/bin ## master...origin/master A  commit.sh parent: d4095ba   0000000..e22f6b9 changes: http://localhost:8080/ipfs/QmVVbPBrfhH52KL43B4yKTYKF8hpa9bUTm23qmkLbsLiPF

---
+++ commit.sh
@@ -0,0 +1,44 @@
+#
+
+# intent: auto-commit ...
+
+date=$(date +%Y-%m-%d)
+ver=$(perl -S version -a $0 | xyml scheduled)
+echo ver: $ver
+commit_msg()
+{
+qm=$(git status -v -uno | ipfs add -Q -pin=false)
+ns=$(perl -S nid.pl "urn:ipfs:$qm")
+msg=$(cat <<EOM
+daily check-in $ver ns:$nid ($date)
+top: $gittop
+`git status -s -b -uno --ignore-submodules=untracked`
+parent: $gitid
+`git --no-pager status -v -b --ignore-submodules=untracked | grep -e '^index ' | sed -e 's/^index/ /'`
+changes: http://localhost:8080/ipfs/$qm
+EOM
+)
+echo "msg: |-"
+echo "$msg" | sed -e 's/^/  /'
+echo '.'
+}
+# ---------------------------------------------------------------------
+git pull
+gittop=$(git rev-parse --show-toplevel) && echo top: $gittop
+gitid=$(git rev-parse --short HEAD)
+gituser
+commit_msg
+git commit -a -uno -m "$msg" --author=$USER
+
+git tag -f -a $ver -m "tagging $gitid on $date"
+# test if tag $ver exist ...
+remote=$(git rev-parse --abbrev-ref @{upstream} |cut -d/ -f 1)
+if git ls-remote --tags | grep "$ver"; then
+  git push --delete $remote "$ver"
+fi
+echo "git push : "
+git push --follow-tags $remote $branch
+echo .
+# ---------------------------------------------------------------------
+true;
+
