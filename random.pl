1ed675ffb06bdafb301c36930ad0ed3aec98367e
Alexander R. Karstetter <akarstetter@healthium.gq>
2021-01-06 06:43:00 +0100

    improved the interface (add option -a and default to only output irnd)

--- random.pl
+++ random.pl
@@ -1,9 +1,38 @@
 #!/usr/bin/perl
 
+
+my $all = 0;
 my $seed=srand();
+
+while (@ARGV && $ARGV[0] =~ m/^-/)
+{
+  $_ = shift;
+  #/^-(l|r|i|s)(\d+)/ && (eval "\$$1 = \$2", next);
+  if (/^-v(?:erbose)?/) { $verbose= 1; }
+  elsif (/^-a(?:ll)?/) { $all= 1; }
+  elsif (/^-y(?:ml)?/) { $yml= 1; }
+  else                  { die "Unrecognized switch: $_\n"; }
+
+}
+#understand variable=value on the command line...
+eval "\$$1='$2'"while $ARGV[0] =~ /^(\w+)=(.*)/ && shift;
+
+my $intent =  sprintf "Get a random number smaller than %s",$ARGV[0]||'max';
+
+my $max = shift;
+my $rnd = rand($max);
+my $irnd = int($rnd);
+
+if ($all || $yml) {
 printf "--- # %s\n",$0;
 printf "seed: %s\n",$seed;
-printf "rand: %s\n",rand($ARGV[0]);
+printf "max: %s\n",$max||'1';
+printf "irnd: %s\n",$irnd;
+printf "rand: %s\n",$rnd;
+printf "intent: %s\n",$intent;
+} else {
+ print $irnd;
+}
 
 1; # $Source: /my/perl/scripts/random.pl$
 
